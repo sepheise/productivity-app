331ea7308fde7fc8f455d9a9bf3759f0b03facd2
Patricio Sepúlveda Heise <54915007+sepheise@users.noreply.github.com>
2022-10-12 16:21:36 -0300

    Ci/setup macos app (#14)
    * chore: Create MacOS SwiftUI app
    
    * chore: Create Xcode workspace
    
    * build: Add new target to CI scheme
    
    * chore: Ignore test results folder
    
    * build: Add framework dependency to App target
    
    * build: Use workspace scheme instead FW project only

---
+++ ProductivityApp/ProductivityApp/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityAppApp.swift
+//  ProductivityApp
+//
+//  Created by Patricio Sepúlveda Heise on 12-10-22.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
